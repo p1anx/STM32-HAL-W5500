c45bf37c52776e798282e6125b49eefe6ddef00b
Xu Wenjing <80949489+p1anx@users.noreply.github.com>
2024-10-29 00:39:28 +0800

    Update MyUDP.h

--- Core/Src/MyUDP.h
+++ Core/Src/MyUDP.h
@@ -15,7 +15,7 @@ extern uint8_t UDP_send_buff[128];   //定义UDP发送缓冲区
 extern uint8_t remote_ip[4]; //远程IP地址
 extern uint16_t remote_port; //远程端口号
 
-void UDPinit(void);
+void W5500_Init(void);
 void do_udp(void);
 void UDP_send(uint8_t* data,uint8_t len);
 void Analysis(uint8_t *buf);
